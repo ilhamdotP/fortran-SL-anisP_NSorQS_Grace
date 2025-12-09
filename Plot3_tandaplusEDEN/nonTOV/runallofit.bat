5ac1ba0dc712d502f61cae61b2a1532acbdf752c
ilhamdotP <trickymindful@yahoo.com>
2025-12-09 11:22:20 +0700

    Add files via upload

---
+++ Plot3_tandaplusEDEN/nonTOV/runallofit.bat
@@ -0,0 +1,43 @@
+cd ..
+cd nonTOV/"0-B=57,BT=57,a4=0.7,a4T=0.7,w=0.333,ct=1e0"
+mkdir file8
+ tesGrace.exe
+cd ../"1-B=92,BT=57,a4=0.7,a4T=0.7,w=0.333,ct=1e0"
+mkdir file9
+ tesGrace.exe
+cd ../"2-B=57,BT=92,a4=0.7,a4T=0.7,w=0.333,ct=1e0"
+mkdir file10
+ tesGrace.exe
+cd ../"3-B=92,BT=92,a4=0.7,a4T=0.7,w=0.333,ct=1e0"
+mkdir file11
+ tesGrace.exe
+cd ../"4-B=57,BT=57,a4=0.7,a4T=0.07,w=0.333,ct=1e0"
+mkdir file12
+ tesGrace.exe
+cd ../"5-B=57,BT=57,a4=0.7,a4T=0.1,w=0.333,ct=1e0"
+mkdir file13
+ tesGrace.exe
+cd ../"6-B=57,BT=57,a4=0.7,a4T=0.7,w=1,ct=1e0"
+mkdir file14
+ tesGrace.exe
+cd ../"7-B=57,BT=57,a4=0.7,a4T=0.7,w=0.333,ct=1e5"
+mkdir file15
+ tesGrace.exe
+cd ../"8-B=57,BT=57,a4=0.7,a4T=0.7,w=0.333,ct=1e6"
+mkdir file16
+ tesGrace.exe
+cd ../"9-B=57,BT=57,a4=0.7,a4T=0.7,w=0.333,ct=1e7"
+mkdir file17
+ tesGrace.exe
+cd ../"10-B=57,BT=57,a4=0.1,a4T=0.7,w=0.333,ct=1e0"
+mkdir file 18 
+ tesGrace.exe
+cd ../"11-B=57,BT=57,a4=0.1,a4T=0.1,w=0.333,ct=1e0"
+mkdir file 19 
+a.exe
+cd ../"12-B=57,BT=57,a4=0.07,a4T=0.7,w=0.333,ct=1e0"
+mkdir file 20
+ tesGrace.exe
+cd ../"13-B=57,BT=57,a4=0.07,a4T=0.07,w=0.333,ct=1e0"
+mkdir file 21
+ tesGrace.exe
